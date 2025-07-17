d54fbeab9e63cb0162ca26f2672bd5c451fae8f7
Jatan06 <parikhjatan54@gmail.com>
2025-07-17 21:15:19 +0530

    FOS Files commiting

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-class main{
+import Menus.MainMenu;
+class Main {
+    static {
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tSwadkart - Food Ordering System\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tEk command, anek swaad!");
+    }
     public static void main(String[] args) {
-        System.out.println("MAT KARO!!!!!!!");
+        Main.run();
+    }
+    static void run() {
+        MainMenu.show();
     }
 }
--- src/Menus/MainMenu.java
+++ src/Menus/MainMenu.java
@@ -1,4 +1,33 @@
 package Menus;
-
+import java.util.*;
 public class MainMenu {
+    static boolean b;
+    static Scanner s = new Scanner(System.in);
+    public static void show() {
+        try {
+            b = true;
+            while (b) {
+                System.out.print("\nEnter \"" + 1 + "\" for User\nEnter \"" + 2 + "\" for Admin\nEnter \"" + 3 + "\" for Exit\n\nEnter :- ");
+                int n = s.nextInt();
+                switch (n) {
+                    case 1:
+                        System.out.println("\nWelcome User.");
+                        break;
+                    case 2:
+                        System.out.println("\nWelcome Admin.");
+                        break;
+                    case 3:
+                        System.out.println("\nSwadKart signing off... pet full, mood chill! \uD83D\uDE0E\uD83C\uDF55");
+                        b = false;
+                        break;
+                    default:
+                        System.out.println("Invalid Choice Please try again !");
+                        break;
+                }
+            }
+        }
+        catch (Exception e) {
+
+        }
+    }
 }
